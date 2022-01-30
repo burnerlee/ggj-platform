cd859e78680b616250cf019e00d7909c19a7d119
burnerlee <avi.aviral140@gmail.com>
2022-01-30 16:36:48 +0530

    add ui

--- src/UserInterface/CoinsCounter.gd
+++ src/UserInterface/CoinsCounter.gd
@@ -16,7 +16,7 @@ func _ready():
 		_level_node.get_node("Player1").connect("collect_coin", self, "_collect_coin")
 		_level_node.get_node("Player2").connect("collect_coin", self, "_collect_coin")
 	else:
-		var _player_path = get_parent().get_child(1)
+		var _player_path = get_parent().get_parent()
 		_player_path.connect("collect_coin", self, "_collect_coin")
 
 
